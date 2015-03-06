a34a4dd93169188fec4902d554595d1f3e4ac128
Mark Y. Thompson Jr <moomoomamoo@gmail.com>
2015-03-05 21:20:59 -0700

    url updated

--- GoingSocial/main/urls.py
+++ GoingSocial/main/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from views.views import home, splash, about, SignUp
+from views.views import splash, about, sign_up
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'main.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', splash, name='splash'),
-    url(r'^home', home, name='home'),
+    #url(r'^home', home, name='home'),
     url(r'^about', about, name='about'),
-    url(r'^SignUp', SignUp, name='home'),
-    url(r'^User/(?P<thanks_id>\d+)/$', User, name='user'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^SignUp', sign_up, name='signup'),
+    #url(r'^User/(?P<thanks_id>\d+)/$', User, name='user'),
+    #url(r'^admin/', include(admin.site.urls)),
 )
