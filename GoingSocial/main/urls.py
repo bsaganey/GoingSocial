da79e74975da699d213f490376a28e4f3c3598bc
Mark Y. Thompson Jr <moomoomamoo@gmail.com>
2015-03-05 21:42:49 -0700

    linked up the sign up page

--- GoingSocial/main/urls.py
+++ GoingSocial/main/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
-from views.views import splash, about, sign_up, home
+from views.views import splash, home, about, sign_up
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^$', splash, name='splash'),
     url(r'^home', home, name= 'home'),
     url(r'^about', about, name='about'),
-    url(r'^SignUp', sign_up, name='signup'),
+    url(r'^sign_up', sign_up, name='sign_up'),
     #url(r'^User/(?P<thanks_id>\d+)/$', User, name='user'),
     #url(r'^admin/', include(admin.site.urls)),
 )
